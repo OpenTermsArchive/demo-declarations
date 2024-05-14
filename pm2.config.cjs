f65523463920b90ebe6da063b99474a117d72b54
Nicolas Dupont <npg.dupont@gmail.com>
2024-05-14 16:52:17 +0200

    wip

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -22,9 +22,9 @@ module.exports = {
       restart_delay: 15 * 60 * 1000,
     },
     {
-      name: 'ota-federated-api',
+      name: 'ota-federation-api',
       script: 'npm',
-      args: 'run start:federated-api',
+      args: 'run start:federation-api',
       max_restarts: 2,
       min_uptime: '1h', // Set a relatively high duration (more than the longest run) so that restarts that occur before this duration has elapsed are considered unstable.
       restart_delay: 15 * 60 * 1000,
